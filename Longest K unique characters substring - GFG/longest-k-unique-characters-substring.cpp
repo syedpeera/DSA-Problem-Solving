b1fbea91b812c313616ea31e23a748f14598664a
syed peera saheb <peeraandroid.123@gmail.com>
2023-03-24 12:35:17 +0530

    Added solution - LeetHub

---
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int i=0;
+        int j=0;
+        int n=s.size();
+        int maxLen=-1;
+        unordered_map<char, int> m;
+        while(j<n){
+            m[s[j]]++;
+            if(m.size()<k){
+                j++;
+            }
+            else if(m.size()==k){
+                maxLen=max(maxLen,j-i+1);   
+                j++;
+            }
+            else if(m.size()>k){
+                while(m.size()>k){
+                    m[s[i]]--;
+                    if(m[s[i]]==0){
+                        m.erase(s[i]);
+                    }
+                    i++;
+                }
+                if(m.size()==k){
+                    maxLen=max(maxLen,j-i+1);
+                    j++;
+                }
+            }
+        }
+        return maxLen;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+        Solution ob;
+        cout << ob.longestKSubstr(s, k) << endl;
+    }
+}
+
+// } Driver Code Ends
