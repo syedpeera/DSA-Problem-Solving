5956fe146b2320a80bba60b8a92e1b3f15e2a4d9
syed peera saheb <peeraandroid.123@gmail.com>
2023-03-31 00:20:57 +0530

    Time: 64 ms (6.49%), Space: 23.3 MB (5.60%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<=right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
+        for(int i=0;i<s.size();i++){
+            cout<<s[i];
+        }
+    }
+};
