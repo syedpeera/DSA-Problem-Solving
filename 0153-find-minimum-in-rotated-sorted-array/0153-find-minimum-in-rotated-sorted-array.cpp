ba38b3f148d6da545ab36f33ec9c324cbae53bd6
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-06 15:11:04 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (32.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int findMin(vector<int>& arr) {
-        int n=arr.size();
-        int low = 0, high = n - 1;
-    if (arr[low] <= arr[high])
-        return arr[low];
-    /*returns 0 if array is already sorted*/
-    while (low <= high) {
- 
-        // if first element is mid or
-        // last element is mid
-        // then simply use modulo so it
-        // never goes out of bound.
-        int mid = low + (high - low) / 2;
-        int prev = (mid - 1 + n) % n;
-        int next = (mid + 1) % n;
- 
-        if (arr[mid] <= arr[prev] && arr[mid] <= arr[next])
-            return arr[mid];
-        else if (arr[mid] <= arr[high])
-            high = mid - 1;
-        else if (arr[mid] >= arr[0])
-            low = mid + 1;
-    }
-    return 0;
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            if(nums[left]<=nums[right]){
+                return nums[left];
+            }
+            int mid=left+(right-left)/2;
+            int next=(mid+1)%n;
+            int prev=(mid-1+n)%n;
+            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next]){
+                return nums[mid];
+            }
+            else if(nums[left]<=nums[mid] && nums[mid]>=nums[right]){
+                left=mid+1;
+            }
+            else if(nums[left]>=nums[mid]&& nums[mid]<=nums[right]){
+                right=mid-1;
+            }
+        }
+        return 0;
     }
 };
