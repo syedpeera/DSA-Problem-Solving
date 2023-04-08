aa147894057174ac5c4c5d48e14518b6f551d50c
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-08 10:42:03 +0530

    Time: 21 ms (25.87%), Space: 15.9 MB (52.16%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -7,7 +7,6 @@ public:
         }
         int left=0;
         int right=n-1;
-        int index=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(letters[mid]==target){
@@ -17,10 +16,9 @@ public:
                 left=mid+1;
             }
             else{
-                index=mid;
                 right=mid-1;
             }
         }
-        return letters[index];
+        return letters[left];
     }
 };
