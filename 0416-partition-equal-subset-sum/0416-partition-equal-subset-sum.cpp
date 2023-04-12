75626b9d6903dae423caf6b6fda075ac2b378b7a
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-12 14:24:47 +0530

    Time: 274 ms (67.40%), Space: 9.9 MB (86.26%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -19,30 +19,21 @@ public:
     }
     
     bool isSubsetSum(vector<int> nums, int n, int sum) {
-        
-        int dp[n+1][sum+1];
-        for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<sum+1;j++)
-            {
-                if(i==0){
-                    dp[i][j]=false;
-                }else if(j==0){
-                    dp[i][j]=true;
-                }
-            }
-        }
-        
+        vector<bool> prev(sum+1, false);
+        vector<bool> curr(sum+1, false);
+        prev[0]=true;
+        curr[0]=true;
         for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
                 if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                    curr[j] = prev[j-nums[i-1]] || prev[j];
                 }
                 else if(nums[i-1]>j){
-                    dp[i][j] = dp[i-1][j]; 
+                    curr[j] = prev[j]; 
                 }
             }
+            prev=curr;
         }
-        return dp[n][sum];
+        return prev[sum];
     }
 };
