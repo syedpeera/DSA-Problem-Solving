241e47a023cd70bbf8dcb0e877cf877ca936b06a
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-12 11:05:23 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,20 +8,24 @@ class Solution
 {
     public:
     int solveKnapSack(vector<vector<int>>& dp, int wt[], int val[], int w, int n){
-        if(n==0 || w==0){
-            return 0;
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<w+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
         }
-        if(dp[n][w]!=-1){
-            return dp[n][w];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<w+1;j++){
+                if(wt[i-1]<=j){
+                    dp[i][j] = max((val[i-1]+dp[i-1][j-wt[i-1]]), (dp[i-1][j])); 
+                }
+                else if(wt[i-1]>j){
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        if(wt[n-1]<=w){
-            dp[n][w] = max((val[n-1]+solveKnapSack(dp, wt, val, w-wt[n-1], n-1)), solveKnapSack(dp, wt, val, w, n-1));
-            return dp[n][w];
-        }
-        else if(wt[n-1]>w){
-            dp[n][w] = solveKnapSack(dp, wt, val, w, n-1);
-            return dp[n][w];
-        }
+        return dp[n][w];
     }
     int knapSack(int w, int wt[], int val[], int n) 
     { 
