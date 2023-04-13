ce4c27d67a9922fa2c39a08ce2c9cdac18da6ddf
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-13 16:34:30 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,30 +9,22 @@ using namespace std;
 
 class Solution{
 public:
-    int solveKnapSack(vector<vector<int>>& dp, int n, int w, int val[], int wt[]){
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<w+1; j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
+    int knapSack(int n, int w, int val[], int wt[])
+    {
+        vector<int> prev(w+1,0);
+        vector<int> curr(w+1,0);
         for(int i=1; i<n+1; i++){
             for(int j=1; j<w+1; j++){
                 if(wt[i-1]<=j){
-                    dp[i][j] = max( (val[i-1]+dp[i][j-wt[i-1]]), dp[i-1][j]);
+                    curr[j] = max( (val[i-1]+curr[j-wt[i-1]]), prev[j]);
                 }
                 if(wt[i-1]>j){
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev=curr;
         }
-        return dp[n][w];
-    }
-    int knapSack(int n, int w, int val[], int wt[])
-    {
-        vector<vector<int>> dp(n+1, vector<int>(w+1, -1));
-        return solveKnapSack(dp, n, w, val, wt);
+        return prev[w];
     }
 };
 
