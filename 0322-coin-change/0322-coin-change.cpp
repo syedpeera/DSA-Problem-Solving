8601c9666f40e1e9cad2f1f7222695950f35efa2
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-14 17:20:06 +0530

    Time: 176 ms (14.58%), Space: 50.2 MB (9.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
+    int solveCoinChange(vector<vector<int>>& dp, vector<int>& coins, int n, int sum){
+        if(n==0){
+            return  INT_MAX-1;
+        }
+        if(sum==0){
+            return  0;
+        }
+        if(dp[n][sum]!=-1){
+            return dp[n][sum];
+        }
+        if(coins[n-1]<=sum){
+            dp[n][sum] = min(1+solveCoinChange(dp, coins, n, sum-coins[n-1]), solveCoinChange(dp, coins, n-1, sum));
+            return dp[n][sum];
+        }
+        else{
+            dp[n][sum] = solveCoinChange(dp, coins, n-1, sum);
+            return dp[n][sum];
+        }
+    }
     int coinChange(vector<int>& coins, int sum) {
         int n=coins.size();
-        vector<int> prev(sum+1, INT_MAX-1);
-        vector<int> curr(sum+1, 0);
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<sum+1; j++){
-                if(j==0){
-                    curr[j]=0;
-                }
-                if(i==0){
-                    curr[j]=INT_MAX-1;
-                }
-            }
-        }
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<sum+1; j++){
-                if(coins[i-1]<=j){
-                    curr[j] = min(1+curr[j-coins[i-1]], prev[j]);
-                }
-                if(coins[i-1]>j){
-                    curr[j] = prev[j];
-                }
-            }
-            prev=curr;
-        }
-        if(prev[sum]>=INT_MAX-1){
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+        int value = solveCoinChange(dp, coins, n, sum);
+        if(value>=INT_MAX-1){
             return -1;
         }
-        return prev[sum];
+        else{
+            return value;
+        }
     }
 };
