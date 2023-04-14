67a36a1e52dc9aa8e530483cae799f6d58838f5a
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-14 16:10:40 +0530

    Time: 215 ms (9.38%), Space: 49.4 MB (14.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,15 +13,7 @@ public:
                 }
             }
         }
-        for(int j=1;j<sum+1;j++){
-            if(j%coins[0]==0){
-                dp[1][j]=j/coins[0]; 
-            }
-            else{
-                dp[1][j]=INT_MAX-1;
-            }
-        }
-        for(int i=2; i<n+1; i++){
+        for(int i=1; i<n+1; i++){
             for(int j=1; j<sum+1; j++){
                 if(coins[i-1]<=j){
                     dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
