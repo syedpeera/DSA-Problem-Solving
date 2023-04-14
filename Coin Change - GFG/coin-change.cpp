6595bc891e4fe59845360570b37636200ef77ed7
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-14 12:21:46 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -6,28 +6,22 @@ using namespace std;
 class Solution {
   public:
     long long int count(int coins[], int n, int sum) {
-        vector<vector<long long int>> dp(n+1, vector<long long int>(sum+1, -1));
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<sum+1; j++){
-                if(i==0){
-                    dp[i][j]=0;
-                }
-                if(j==0){
-                    dp[i][j]=1;
-                }
-            }
-        }
+        vector<long long int> prev(sum+1, 0);
+        vector<long long int> curr(sum+1, 0);
+        prev[0]=1;
+        curr[0]=1;
         for(int i=1; i<n+1; i++){
             for(int j=1; j<sum+1; j++){
                 if(coins[i-1]<=j){
-                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                    curr[j] = curr[j-coins[i-1]] + prev[j];
                 }
                 if(coins[i-1]>j){
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev=curr;
         }
-        return dp[n][sum];
+        return prev[sum];
     }
 };
 
