c2c7811d370b97cbbae71e77d08b096d809d44bf
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-14 16:19:48 +0530

    Time: 134 ms (33.44%), Space: 18.6 MB (41.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int sum) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+        vector<int> prev(sum+1, INT_MAX-1);
+        vector<int> curr(sum+1, 0);
         for(int i=0; i<n+1; i++){
             for(int j=0; j<sum+1; j++){
                 if(j==0){
-                    dp[i][j]=0;
+                    curr[j]=0;
                 }
                 if(i==0){
-                    dp[i][j]=INT_MAX-1;
+                    curr[j]=INT_MAX-1;
                 }
             }
         }
         for(int i=1; i<n+1; i++){
             for(int j=1; j<sum+1; j++){
                 if(coins[i-1]<=j){
-                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                    curr[j] = min(1+curr[j-coins[i-1]], prev[j]);
                 }
                 if(coins[i-1]>j){
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev=curr;
         }
-        if(dp[n][sum]>=INT_MAX-1){
+        if(prev[sum]>=INT_MAX-1){
             return -1;
         }
-        return dp[n][sum];
+        return prev[sum];
     }
 };
