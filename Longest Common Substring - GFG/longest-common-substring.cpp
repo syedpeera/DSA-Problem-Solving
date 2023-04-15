9f6ce423d34c072cec9e8d609e10ee9a89bb712d
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-15 11:50:03 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -15,21 +15,15 @@ class Solution{
                 }
             }
         }
+        int maxVal=0;
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
                 if(s1[i-1]==s2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
+                    maxVal=max(maxVal, dp[i][j]);
                 }
                 else{
                     dp[i][j]=0;
-                }
-            }
-        }
-        int maxVal=INT_MIN;
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(dp[i][j]>maxVal){
-                    maxVal=dp[i][j];
                 }
             }
         }
