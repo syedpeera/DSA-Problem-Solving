5eddda05b9850bfb7bb50d82638a3e4f0f3cfb70
syed peera saheb <peeraandroid.123@gmail.com>
2023-04-18 17:30:48 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int LCS(string s1, string s2, int n, int m){
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int LPS(string s){
+        int n=s.size();
+        int m=n;
+        string x=s;
+        reverse(x.begin(), x.end());
+        return LCS(s, x, n, m);
+    }
+    int findMinInsertions(string s){
+        int n=s.size();
+        return n-LPS(s);
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.findMinInsertions(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
