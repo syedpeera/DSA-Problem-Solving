323405ca454ccffd945b78313ce40302d12c0bfe
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-09 11:33:07 +0530

    Time: 25 ms (58.68%), Space: 13.8 MB (61.03%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool dfs(int v, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& callStack) {
+        visited[v] = true;
+        callStack[v] = true;
+        vector<int>& neighbors = graph[v];
+        for (int neighbor : neighbors) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, graph, visited, callStack)) {
+                    return true;
+                }
+            } else if (callStack[neighbor]) {
+                return true;
+            }
+        }
+        callStack[v] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        for (vector<int>& prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+            graph[course].push_back(prerequisiteCourse);
+        }
+        vector<bool> visited(numCourses, false);
+        vector<bool> callStack(numCourses, false);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (dfs(i, graph, visited, callStack)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
