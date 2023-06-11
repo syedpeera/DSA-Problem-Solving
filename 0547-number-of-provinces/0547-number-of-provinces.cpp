d4a1747bbf2d609cd23d8e639d782fbd263fbb2e
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-11 19:10:27 +0530

    Time: 28 ms (54.90%), Space: 16.6 MB (5.18%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+    /*
     void dfs(int v, vector<vector<int>>& adj, vector<bool>& visited){
         visited[v]=true;
         vector<int> neighbors = adj[v];
         for(int neighbor: neighbors){
             if(!visited[neighbor]){
                 dfs(neighbor, adj, visited);
+            }
+        }
+    }*/
+    void bfs(int v, vector<vector<int>>& adj, vector<bool>& visited){
+        queue<int> q;
+        q.push(v);
+        visited[v]=true;
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            for(int neighbor: adj[curr]){
+                if(!visited[neighbor]){
+                    q.push(neighbor);
+                    visited[neighbor]=true;
+                }
             }
         }
     }
@@ -21,9 +37,16 @@ public:
         }
         vector<bool> visited(n, false);
         int count=0;
+        /*
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 dfs(i, adj, visited);
+                count++;
+            }
+        }*/
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                bfs(i, adj, visited);
                 count++;
             }
         }
