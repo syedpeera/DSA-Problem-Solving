7bdc735981790280af788fa9900688672e26bb3b
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-19 13:10:51 +0530

    Time: 1 ms (95.01%), Space: 43.6 MB (29.79%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void generateParenthesis(List<String> result, String current, int open, int close, int n){
+        if(current.length()==2*n){
+            result.add(current);
+        }
+        if(open < n){
+            generateParenthesis(result, current+"(", open+1, close, n);
+        }
+        if(close < open){
+            generateParenthesis(result, current+")", open, close+1, n);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generateParenthesis(result, "", 0, 0, n);   
+        return result;
+    }
+}
