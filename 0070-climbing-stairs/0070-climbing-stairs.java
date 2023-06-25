8ef95f9215b624ec2eb76caf2b7a5c94ae5a026c
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-25 19:41:32 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (67.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n==1){
+            return 1;
         }
-        return dp[n];
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr=0;
+        for(int i=2;i<=n;i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return curr;
     }
 }
