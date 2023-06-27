42767c004b2a77cd496a4b6cba09cb563a50bb82
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-27 22:10:33 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (8.36%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left=0;
+        int mid=0;
+        int right=nums.size()-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left], nums[mid]);
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[right]);
+                right--;
+            }
+        }
+    }
+};
