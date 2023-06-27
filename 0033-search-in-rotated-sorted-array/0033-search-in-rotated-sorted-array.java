f83363a7770f94158d305f20bafb31b9a2ff6a0d
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-27 21:52:24 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (56.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,50 +1,29 @@
 class Solution {
-    int binarySearch(int nums[], int target, int left, int right){
-        int n=nums.length;
+    int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
         while(left<=right){
-            int mid=left+(right-left)/2;
+            int mid = left + (right-left)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]<target){
-                left=mid+1;
+            else if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<=nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
             }
             else{
-                right=mid-1;
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
             }
         }
         return -1;
-    }
-    int findPivot(int nums[], int target){
-        int n=nums.length;
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int next=(mid+1)%n;
-            int prev=(mid-1+n)%n;
-            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next]){
-                return mid;
-            }
-            else if(nums[left]<=nums[mid] && nums[mid]>=nums[right]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
-        }
-        return -1;
-    }
-    int search(int[] nums, int target) {
-        int n=nums.length;
-        int index = findPivot(nums, target); // index of the minimum element
-        int leftSearch = binarySearch(nums, target, 0, index-1);
-        int rightSearch = binarySearch(nums, target, index, n-1);
-        if(leftSearch==-1){
-            return rightSearch;
-        }
-        else{
-            return leftSearch;
-        }
     }
 }
