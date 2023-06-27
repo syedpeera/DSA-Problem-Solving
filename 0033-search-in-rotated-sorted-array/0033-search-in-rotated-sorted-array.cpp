af0edc4c8763275c1a15eb230a07553bbeca2d4c
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-27 21:57:21 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (51.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, int left, int right){
-        int n=nums.size();
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]<target){
-                left=mid+1;
+            else if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<=nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
             }
             else{
-                right=mid-1;
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
             }
         }
         return -1;
-    }
-    int findPivot(vector<int>& nums, int target){
-        int n=nums.size();
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int next=(mid+1)%n;
-            int prev=(mid-1+n)%n;
-            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next]){
-                return mid;
-            }
-            else if(nums[left]<=nums[mid] && nums[mid]>=nums[right]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
-        }
-        return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int index = findPivot(nums, target); // index of the minimum element
-        int leftSearch = binarySearch(nums, target, 0, index-1);
-        int rightSearch = binarySearch(nums, target, index, n-1);
-        if(leftSearch==-1){
-            return rightSearch;
-        }
-        else{
-            return leftSearch;
-        }
     }
 };
