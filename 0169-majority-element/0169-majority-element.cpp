85a548165626f864aed755512b6de500ce3692ca
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-28 20:13:02 +0530

    Time: 18 ms (80.98%), Space: 19.5 MB (56.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,12 +5,12 @@ public:
         int count = 0;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            if(count == 0){
+            if(nums[i]==candidate){
+                count++;
+            }
+            else if(count == 0){
                 candidate = nums[i];
                 count=1;
-            }
-            else if(candidate == nums[i]){
-                count++;
             }
             else{
                 count--;
