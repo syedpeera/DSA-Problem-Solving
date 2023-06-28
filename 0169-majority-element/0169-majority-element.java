3f6ee8fd3789e389c2808c76cc6fbb5e47effad8
syed peera saheb <peeraandroid.123@gmail.com>
2023-06-28 20:12:35 +0530

    Time: 1 ms (99.77%), Space: 48.2 MB (52.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,12 +4,12 @@ class Solution {
         int count = 0;
         int n = nums.length;
         for(int i=0;i<n;i++){
-            if(count == 0){
+            if(nums[i]==candidate){
+                count++;
+            }
+            else if(count == 0){
                 candidate = nums[i];
                 count=1;
-            }
-            else if(nums[i]==candidate){
-                count++;
             }
             else{
                 count--;
