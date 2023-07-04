da95b100975afe4cb14e2516432de11f4a3545a9
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-04 22:27:36 +0530

    Time: 1 ms (100.00%), Space: 59.1 MB (37.95%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,12 +1,15 @@
 class Solution {
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public int firstMissingPositive(int[] nums) {
         int n=nums.length;
         int i=0;
         while(i<n){
             if(nums[i]>0 && nums[i]<n && nums[i]!=nums[nums[i]-1]){
-                int temp = nums[i];
-                nums[i] = nums[temp-1];
-                nums[temp-1] = temp;
+                swap(nums, i, nums[i]-1);
             }
             else{
                 i++;
