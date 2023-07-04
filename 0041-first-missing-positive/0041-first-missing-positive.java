9903acb167baad6734f238e715be75915da0cf15
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-04 22:26:03 +0530

    Time: 1 ms (100.00%), Space: 59.4 MB (25.57%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        while(i<n){
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+                int temp = nums[i];
+                nums[i] = nums[temp-1];
+                nums[temp-1] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(nums[j]!=j+1){
+                return j+1;
+            }
+        }
+        return n+1;
+    }
+}
