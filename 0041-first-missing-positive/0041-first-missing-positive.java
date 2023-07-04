243b23eb679dac9af34ce3868e564a79321a3c2b
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-04 22:26:20 +0530

    Time: 1 ms (100.00%), Space: 59.3 MB (29.10%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int i=0;
         while(i<n){
-            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+            if(nums[i]>0 && nums[i]<n && nums[i]!=nums[nums[i]-1]){
                 int temp = nums[i];
                 nums[i] = nums[temp-1];
                 nums[temp-1] = temp;
