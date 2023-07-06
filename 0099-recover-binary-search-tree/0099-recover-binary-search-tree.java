1c6d84323fe6698e96b093469990036d72b6c157
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-06 13:40:00 +0530

    Time: 2 ms (100.00%), Space: 43.5 MB (34.57%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,40 +14,45 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root, TreeNode[] prev, TreeNode[] first, TreeNode[] middle, TreeNode[] last) {
-        if (root == null) {
+    private TreeNode prev;
+    private TreeNode first;
+    private TreeNode middle;
+    private TreeNode last;
+    public void inorder(TreeNode root){
+        if(root==null){
             return;
         }
-        inorder(root.left, prev, first, middle, last);
-        if (prev[0] != null && prev[0].val > root.val) {
-            if (first[0] == null) {
-                first[0] = prev[0];
-                middle[0] = root;
-            } else {
-                last[0] = root;
+        inorder(root.left);
+        if(prev!=null && prev.val > root.val){
+            if(first==null){
+                first = prev;
+                middle = root;
+            }
+            else{
+                last = root;
             }
         }
-        prev[0] = root;
-        inorder(root.right, prev, first, middle, last);
+        prev = root;
+        inorder(root.right);
     }
-
     public void recoverTree(TreeNode root) {
-        if (root == null) {
+        if(root==null){
             return;
         }
-        TreeNode[] prev = {null};
-        TreeNode[] first = {null};
-        TreeNode[] middle = {null};
-        TreeNode[] last = {null};
-        inorder(root, prev, first, middle, last);
-        if (first[0] != null && last[0] != null) {
-            int temp = first[0].val;
-            first[0].val = last[0].val;
-            last[0].val = temp;
-        } else if (first[0] != null && middle[0] != null) {
-            int temp = first[0].val;
-            first[0].val = middle[0].val;
-            middle[0].val = temp;
+        prev = new TreeNode(Integer.MIN_VALUE);
+        first = null;
+        middle = null;
+        last = null;
+        inorder(root);
+        if(first!=null && last!=null){
+            int temp = first.val;
+            first.val = last.val;
+            last.val = temp;
+        }
+        else if(first!=null && middle!=null){
+            int temp = first.val;
+            first.val = middle.val;
+            middle.val = temp;
         }
     }
 }
