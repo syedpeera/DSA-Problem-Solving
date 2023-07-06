e3444d42a8d99362aa842ab429c070ad27477a52
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-06 12:05:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (54.13%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int n = s.size();
+        vector<int> count(26, 0);
+        vector<bool> visited(26, false);
+        stack<char> bucket;
+        for(int i=0;i<n;i++){
+            count[s[i]-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            count[s[i]-'a']--;
+            if(visited[s[i]-'a']){
+                continue;
+            }
+            while(!bucket.empty() && bucket.top()>s[i] && count[bucket.top()-'a']>0){
+                visited[bucket.top()-'a'] = false;
+                bucket.pop();
+            }
+            bucket.push(s[i]);
+            visited[s[i]-'a']=true;
+        }
+        string result = "";
+        while(!bucket.empty()){
+            result.insert(result.begin(), bucket.top());
+            bucket.pop();
+        }
+        return result;
+    }
+};
