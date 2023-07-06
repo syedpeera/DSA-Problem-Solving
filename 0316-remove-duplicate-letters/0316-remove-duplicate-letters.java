3ef50a4b3879e70d5200f7cee0a199d4659fc27c
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-06 11:47:35 +0530

    Time: 4 ms (45.21%), Space: 41.3 MB (80.36%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,36 +1,29 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] count = new int[26];
-        boolean[] visited = new boolean[26];
+        int count[] = new int[26];
+        boolean visited[] = new boolean[26];
         Stack<Character> stack = new Stack<>();
         int n = s.length();
-
-        for (char ch : s.toCharArray()) {
-            count[ch - 'a']++;
+        for(int i=0;i<n;i++){
+            count[s.charAt(i)-'a']++;
         }
-
-        for (char ch : s.toCharArray()) {
-            count[ch - 'a']--;
-
-            if (visited[ch - 'a']) {
+        for(int i=0;i<n;i++){
+            count[s.charAt(i)-'a']--;
+            if(visited[s.charAt(i)-'a']){
                 continue;
             }
-
-            while (!stack.isEmpty() && stack.peek() > ch && count[stack.peek() - 'a'] > 0) {
-                visited[stack.peek() - 'a'] = false;
+            while(!stack.isEmpty() && stack.peek()>s.charAt(i) && count[stack.peek()-'a']>0){
+                visited[stack.peek()-'a'] = false;
                 stack.pop();
             }
-
-            stack.push(ch);
-            visited[ch - 'a'] = true;
+            stack.push(s.charAt(i));
+            visited[s.charAt(i)-'a'] = true;
         }
-
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
+        while(!stack.isEmpty()){
             sb.insert(0, stack.peek());
             stack.pop();
         }
-
         return sb.toString();
     }
 }
