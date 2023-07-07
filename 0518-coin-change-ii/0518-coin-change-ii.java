cac0dc22d40430ac573649fc42d8215da3232ddf
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-07 19:09:27 +0530

    Time: 5 ms (67.50%), Space: 40.1 MB (88.65%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,27 +1,21 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[n+1][amount+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<amount+1;j++){
-                if(i == 0){
-                    dp[i][j] = 0;
-                }
-                if(j == 0){
-                    dp[i][j] = 1;
-                }
-            }
-        }
+        int prev[] = new int[amount+1];
+        int curr[] = new int[amount+1];
+        curr[0] = 1;
+        prev[0] = 1;
         for(int i=1;i<n+1;i++){
             for(int j=1;j<amount+1;j++){
                 if(coins[i-1] <= j){
-                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                    curr[j] = curr[j-coins[i-1]] + prev[j];
                 }
                 else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
-        return dp[n][amount];
+        return prev[amount];
     }
 }
