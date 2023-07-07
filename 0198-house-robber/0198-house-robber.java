162719ce9671c58af9815c47af10bf598d09210c
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-07 21:17:18 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (46.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 0){
-            return 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        for(int i=0;i<nums.length;i++){
+            int temp = prev1;
+            prev1 = Math.max(prev1, prev2+nums[i]);
+            prev2 = temp;
         }
-        int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = nums[0];
-        
-        for(int i=2;i<=n;i++){
-            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i-1]);
-        }
-        return dp[n];
+        return prev1;
     }
 }
