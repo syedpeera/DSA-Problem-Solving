fd5a56c60329956942156442c05fc1a5996beb0a
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-07 19:25:29 +0530

    Time: 29 ms (88.10%), Space: 43.9 MB (94.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,23 +1,19 @@
 class Solution {
-    public int solveLongestCommonSubsequence(String text1, String text2) {
+    public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                    curr[j] = Math.max(curr[j - 1], prev[j]);
                 }
             }
+            prev = curr.clone();
         }
-        return dp[n][m];
-    }
-
-    public int longestCommonSubsequence(String text1, String text2) {
-        return solveLongestCommonSubsequence(text1, text2);
+        return prev[m];
     }
 }
