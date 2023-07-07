fb37c99df2cb2af7ef497e0581ce1b31b44ad6d6
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-07 19:06:11 +0530

    Time: 10 ms (26.55%), Space: 46.2 MB (28.10%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,4 @@
 class Solution {
-    public int solveChange(int coins[], int n, int amount, int dp[][]){
-        if(amount == 0){
-            return 1;
-        }
-        if(n == 0){
-            return 0;
-        }
-        if(dp[n][amount]!=-1){
-            return dp[n][amount];
-        }
-        if(coins[n-1] <= amount){
-            dp[n][amount] = solveChange(coins, n, amount-coins[n-1], dp) + solveChange(coins, n-1, amount, dp);
-            return dp[n][amount];
-        }
-        else{
-            dp[n][amount] = solveChange(coins, n-1, amount, dp);
-            return dp[n][amount];
-        }
-    }
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int dp[][] = new int[n+1][amount+1];
