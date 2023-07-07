faf348591857d7238460626708e5887ccfcc9f0a
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-07 19:05:30 +0530

    Time: 10 ms (26.55%), Space: 46 MB (37.36%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -23,9 +23,24 @@ class Solution {
         int dp[][] = new int[n+1][amount+1];
         for(int i=0;i<n+1;i++){
             for(int j=0;j<amount+1;j++){
-                dp[i][j] = -1;
+                if(i == 0){
+                    dp[i][j] = 0;
+                }
+                if(j == 0){
+                    dp[i][j] = 1;
+                }
             }
         }
-        return solveChange(coins, n, amount, dp);
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
     }
 }
