2fe06ee0c2913254de6091a4767cd43f031654d4
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-07 17:38:03 +0530

    Added solution - LeetHub

---
+++ Combination Sum II - GFG/combination-sum-ii.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] candidates = new int[n];
+            for (int i = 0; i < n; i++) candidates[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            List<List<Integer>> ans = obj.combinationSum2(candidates, k);
+            Collections.sort(ans, new Comparator<List<Integer>>() {
+                @Override   public int compare(List<Integer> a, List<Integer> b) {
+                    int al = a.size();
+                    int bl = b.size();
+                    int min = Math.min(al, bl);
+                    for (int i = 0; i < min; i++) {
+                        if (a.get(i) < b.get(i))
+                            return -1;
+                        else if (a.get(i) > b.get(i))
+                            return 1;
+                    }
+                    if (al < bl)
+                        return -1;
+                    else if (al > bl)
+                        return 1;
+                    return -1;
+                }
+            });
+            System.out.print("[ ");
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print("[ ");
+                for (int j = 0; j < ans.get(i).size(); j++)
+                    System.out.print(ans.get(i).get(j) + " ");
+                System.out.print("]");
+            }
+            System.out.println(" ]");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public void findCombinationSum2(int index, int candidates[], int target, List<List<Integer>> result, List<Integer> ds){
+        if(target == 0){
+            result.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i = index; i < candidates.length; i++){
+            if(i > index && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            if(candidates[i] > target){
+                break;
+            }
+            ds.add(candidates[i]);
+            findCombinationSum2(i+1, candidates, target - candidates[i], result, ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinationSum2(0, candidates, target, result, ds);
+        return result;
+    }
+}
