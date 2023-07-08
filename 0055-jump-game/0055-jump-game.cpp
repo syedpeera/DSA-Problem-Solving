413f6da8c621df49ed9d4a48f2f75e924c439c2c
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-08 23:07:02 +0530

    Time: 66 ms (85.30%), Space: 48.3 MB (73.59%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxReachable = 0;
+        for(int i=0;i<n;i++){
+            if(maxReachable < i){
+                return false;
+            }
+            maxReachable = max(maxReachable, i+nums[i]);
+        }
+        return true;
+    }
+};
