6fc9f0caca79ee0b4c0493cf97ce2a80fb54a3af
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-12 19:59:48 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (7.55%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,7 +15,7 @@ public class Solution {
             return false;
         }
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
         while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
