fcd292ce65e506368d711a3831c8d871b152b7a5
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-17 20:23:41 +0530

    Time: 5 ms (30.91%), Space: 7.8 MB (6.90%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        /* non - overlapping conditions
+           case 1: rec2 is above rec1 (consider y-axis as reference)
+           case 2: rec2 is below rec1 (consider y-axis as reference)
+           case 3: rec2 is to the left side of rec1 (consider x-axis as reference)
+           case 4: rec2 is to the right side of rec1 (consider x-axis as reference)
+        */
+        
+        if((rec2[1] >= rec1[3]) || (rec2[3] <= rec1[1]) || (rec2[2] <= rec1[0]) || (rec2[0] >= rec1[2])){
+            return false;
+        }
+        
+        return true;
+    }
+};
