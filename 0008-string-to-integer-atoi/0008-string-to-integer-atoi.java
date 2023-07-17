d78e437dc491f17d620348682eaea3f4738e94ca
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-17 22:10:51 +0530

    Time: 1 ms (100.00%), Space: 41.5 MB (51.57%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -3,30 +3,30 @@ class Solution {
         int i = 0;
         int n = s.length();
         
-        // Skip leading whitespace
-        while (i < n && s.charAt(i) == ' ') {
+        //skip leading whitespace
+        while(i < n && s.charAt(i) == ' '){
             i++;
         }
         
-        // Check if number is negative or positive
+        //check if number is negative or positive
         int sign = 1;
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = (s.charAt(i++) == '-') ? -1 : 1;
+        if(i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
         }
         
-        // Convert digits to integer
-        int result = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i++) - '0';
-            
-            // Check for overflow
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        //convert digits to integer
+        int number = 0;
+        while(i < n && Character.isDigit(s.charAt(i))){
+            int digit = s.charAt(i) - '0';
+            //check overflow
+            if(number > Integer.MAX_VALUE/10 || (number == Integer.MAX_VALUE/10) && (digit > Integer.MAX_VALUE % 10)){
+                return (sign == 1)? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             
-            result = result * 10 + digit;
+            number = number * 10 + digit;
+            i++;
         }
-        
-        return result * sign;
+        return number * sign;
     }
 }
