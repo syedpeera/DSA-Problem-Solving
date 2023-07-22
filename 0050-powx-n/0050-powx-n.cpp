3e2ca245e11f44926e351eb5b38485095380cf0d
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-22 13:38:32 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (92.60%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1.0;
+        long N = n;
+        if(N < 0){
+            x = 1.0 / x;
+            N = -1 * N;
+        }
+        while(N > 0){
+            if(N % 2 == 1){
+                result = result * x;
+            }
+            x = x * x;
+            N = N / 2.0;
+        }
+        return result;
+    }
+};
