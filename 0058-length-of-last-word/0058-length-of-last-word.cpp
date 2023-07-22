e85adf81aebd5a8957991985337c8083819d94e6
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-22 12:34:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (85.07%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        int i = s.size()-1;
+        while(i >= 0){
+            while(i >= 0 && s[i] == ' '){
+                i--;
+            }
+            while(i >= 0 && s[i] != ' '){
+                count++;
+                i--;
+            }
+            break;
+        }
+        return count;
+    }
+};
