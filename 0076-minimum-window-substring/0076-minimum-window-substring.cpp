dd16492983931c5355aa49fae0856a0dda83b770
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-23 22:04:53 +0530

    Time: 33 ms (45.60%), Space: 7.8 MB (93.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> targetChars;
-    for (char c : t) {
-        targetChars[c]++;
-    }
-
-    int left = 0;
-    int minLen = INT_MAX;
-    int minStart = 0;
-    int requiredChars = targetChars.size();
-
-    for (int right = 0; right < s.length(); right++) {
-        char rightChar = s[right];
-        if (targetChars.count(rightChar)) {
-            targetChars[rightChar]--;
-            if (targetChars[rightChar] == 0) {
-                requiredChars--;
-            }
+        unordered_map<char, int> tFreq;
+        for (int i=0; i<t.size(); i++) {
+            tFreq[t[i]]++;
         }
-
-        while (requiredChars == 0) {
-            if (right - left + 1 < minLen) {
-                minLen = right - left + 1;
-                minStart = left;
-            }
-
-            char leftChar = s[left];
-            if (targetChars.count(leftChar)) {
-                targetChars[leftChar]++;
-                if (targetChars[leftChar] > 0) {
-                    requiredChars++;
+        
+        int left = 0;
+        int minLen = INT_MAX;
+        int required = tFreq.size();
+        int minStart = 0;
+        string result = "";
+        
+        for(int right=0; right<s.size(); right++) {
+            if (tFreq.find(s[right]) != tFreq.end()) {
+                tFreq[s[right]]--;
+                if (tFreq[s[right]] == 0) {
+                    required--;
                 }
             }
-
-            left++;
+            
+            while (required == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    minStart = left;
+                }
+                
+                if (tFreq.find(s[left]) != tFreq.end()) {
+                    tFreq[s[left]]++;
+                    if (tFreq[s[left]] > 0) {
+                        required++;
+                    }
+                }
+                left++;
+            }
         }
-    }
-
-    return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
+        
+        return minLen == INT_MAX? "" : s.substr(minStart, minLen);
     }
 };
