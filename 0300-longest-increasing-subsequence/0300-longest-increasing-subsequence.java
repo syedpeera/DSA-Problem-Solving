3e39bcbf780e029edd3398091b92c292d804ec7f
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-25 00:04:29 +0530

    Time: 7 ms (84.41%), Space: 43 MB (92.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,34 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, 1);
-        int maxLen = 1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
+    public int cielBinarySearch(List<Integer> temp, int target){
+        int left = 0;
+        int right = temp.size()-1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(temp.get(mid) == target){
+                return mid;
             }
-            maxLen = Math.max(maxLen, dp[i]);
+            else if(temp.get(mid) < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
         }
-        return maxLen;
+        return left;
+    }
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+        
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > temp.get(temp.size()-1)){
+                temp.add(nums[i]);
+            }
+            else{
+                int index = cielBinarySearch(temp, nums[i]);
+                temp.set(index, nums[i]);
+            }
+        }
+        return temp.size();
     }
 }
