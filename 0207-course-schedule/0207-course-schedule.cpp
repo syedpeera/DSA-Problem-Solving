6b97380182e2daf1b070eeea28e7c5a0f8bd3955
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-27 19:55:18 +0530

    Time: 12 ms (99.47%), Space: 13.7 MB (67.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    void bfs(int v, vector<vector<int>> &adj, vector<int> &inDegree, vector<int> &topologicalSort){
-        queue<int> q;
-        for(int i=0; i<v; i++){
-            if(inDegree[i] == 0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            topologicalSort.push_back(curr);
-            vector<int> neighbors = adj[curr];
-            for(int neighbor: neighbors){
-                if(--inDegree[neighbor] == 0){
-                    q.push(neighbor);
+    bool dfs(int v, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& callStack) {
+        visited[v] = true;
+        callStack[v] = true;
+        vector<int>& neighbors = graph[v];
+        for (int neighbor : neighbors) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, graph, visited, callStack)) {
+                    return true;
                 }
+            } else if (callStack[neighbor]) {
+                return true;
             }
         }
+        callStack[v] = false;
+        return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
         for (vector<int>& prerequisite : prerequisites) {
             int course = prerequisite[0];
             int prerequisiteCourse = prerequisite[1];
-            graph[course].push_back(prerequisiteCourse);
+            graph[prerequisiteCourse].push_back(course);
         }
-        
-        vector<int> inDegree(numCourses, 0);
-        for(vector<int> list: graph){
-            for(int element: list){
-                inDegree[element]++;
+        vector<bool> visited(numCourses, false);
+        vector<bool> callStack(numCourses, false);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (dfs(i, graph, visited, callStack)) {
+                    return false;
+                }
             }
         }
-        vector<int> topologicalSort;
-        bfs(numCourses, graph, inDegree, topologicalSort);
-        return topologicalSort.size() == numCourses;
+        return true;
     }
 };
