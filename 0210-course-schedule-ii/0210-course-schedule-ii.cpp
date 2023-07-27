33c1316aee558c22edf7c161b827136c548db777
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-27 18:34:10 +0530

    Time: 24 ms (61.87%), Space: 13.8 MB (53.84%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+     void bfs(int v, vector<vector<int>> &adj, vector<int> &inDegree, vector<int> &topologicalSort){
+        queue<int> q;
+        for(int i=0; i<v; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            topologicalSort.push_back(curr);
+            vector<int> neighbors = adj[curr];
+            for(int neighbor: neighbors){
+                if(--inDegree[neighbor] == 0){
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        for (vector<int>& prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+            graph[prerequisiteCourse].push_back(course);
+        }
+        
+        vector<int> inDegree(numCourses, 0);
+        for(vector<int> list: graph){
+            for(int element: list){
+                inDegree[element]++;
+            }
+        }
+        vector<int> topologicalSort;
+        bfs(numCourses, graph, inDegree, topologicalSort);
+        if(topologicalSort.size() == numCourses){
+            return topologicalSort;
+        }
+        return {};
+    }
+};
