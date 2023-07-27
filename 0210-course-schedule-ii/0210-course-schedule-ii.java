be13dd27d8ec74d414dd9168bdb203d5e7b922aa
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-27 18:41:26 +0530

    Time: 6 ms (63.86%), Space: 45.1 MB (23.69%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void bfs(int v, ArrayList<ArrayList<Integer>> adj, int inDegree[], ArrayList<Integer> topologicalSort){
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<v; i++){
+            if(inDegree[i] == 0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            topologicalSort.add(curr);
+            ArrayList<Integer> neighbors = adj.get(curr);
+            for(int neighbor: neighbors){
+                if(--inDegree[neighbor] == 0){
+                    q.add(neighbor);
+                }
+            }
+        }
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] prerequisite: prerequisites){
+            int course = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+            graph.get(prerequisiteCourse).add(course);
+        }
+        
+        int inDegree[] = new int[numCourses];
+        for(ArrayList<Integer> list: graph){
+            for(int element: list){
+                inDegree[element]++;
+            }
+        }
+        ArrayList<Integer> topologicalSort = new ArrayList<>();
+        bfs(numCourses, graph, inDegree, topologicalSort);
+        if(topologicalSort.size() == numCourses){
+            int[] result = new int[numCourses];
+            for(int i = 0; i < numCourses; i++){
+                result[i] = topologicalSort.get(i);
+            }
+            return result;
+        }
+        int result[] = {};
+        return result;
+    }
+}
