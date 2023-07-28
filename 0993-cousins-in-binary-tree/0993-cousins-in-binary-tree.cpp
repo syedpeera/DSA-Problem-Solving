1dbc5397d30003190bf49538f26edbca2a352ea5
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-28 12:14:05 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (36.25%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    bool isCousins(TreeNode* root, int x, int y) {
-        int xDepth = -1, yDepth = -1;
-        TreeNode* xParent = nullptr, *yParent = nullptr;
-        findDepthAndParent(root, nullptr, x, y, 0, xDepth, yDepth, xParent, yParent);
-        return xDepth == yDepth && xParent != yParent;
-    }
-    void findDepthAndParent(TreeNode* node, TreeNode* parent, int x, int y, int depth,
-                            int& xDepth, int& yDepth, TreeNode*& xParent, TreeNode*& yParent) {
-        if (node == nullptr) return;
-
-        if (node->val == x) {
+    void findDepthAndParent(TreeNode* root, TreeNode* parent, int x, int y, int depth, int &xDepth, int &yDepth, TreeNode* &xParent, TreeNode* &yParent){
+        if(root == nullptr){
+            return;
+        }
+        
+        if(root->val == x){
             xDepth = depth;
             xParent = parent;
-        } else if (node->val == y) {
+        }
+        else if(root->val == y){
             yDepth = depth;
             yParent = parent;
         }
-
-        findDepthAndParent(node->left, node, x, y, depth + 1, xDepth, yDepth, xParent, yParent);
-        findDepthAndParent(node->right, node, x, y, depth + 1, xDepth, yDepth, xParent, yParent);
+        
+        findDepthAndParent(root->left, root, x, y, depth+1, xDepth, yDepth, xParent, yParent);
+        findDepthAndParent(root->right, root, x, y, depth+1, xDepth, yDepth, xParent, yParent);
+    }
+    
+    bool isCousins(TreeNode* root, int x, int y) {
+        int xDepth = -1;
+        int yDepth = -1;
+        TreeNode* xParent = nullptr;
+        TreeNode* yParent = nullptr;
+        findDepthAndParent(root, nullptr, x, y, 0, xDepth, yDepth, xParent, yParent);
+        
+        return xDepth == yDepth && xParent != yParent;
     }
 };
