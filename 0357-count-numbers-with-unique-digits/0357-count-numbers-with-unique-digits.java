b24920652b7f608cfd13d7a0c83931c4d32cc6d1
syed peera saheb <peeraandroid.123@gmail.com>
2023-07-28 15:56:50 +0530

    Time: 0 ms (100.00%), Space: 39 MB (84.26%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n == 0){
+            return 1;
+        }
+        int result = 10;
+        int uniqueDigits = 9;
+        int availableNumbers = 9;
+        while(n > 1 && availableNumbers > 0){
+            uniqueDigits = uniqueDigits * availableNumbers;
+            result = result + uniqueDigits;
+            availableNumbers--;
+            n--;
+        }
+        return result;
+    }
+}
