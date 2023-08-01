0855b9405d93e405dd26c3c8f003b37aae76ce0c
syed peera saheb <peeraandroid.123@gmail.com>
2023-08-01 17:55:31 +0530

    Time: 117 ms (99.86%), Space: 98.5 MB (96.83%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        //store index of element in stack
-        stack<int> bucket;
-        vector<int> result;
         int n = temperatures.size();
+        int hottest = INT_MIN;
+        vector<int> result(n, 0);
         for(int i=n-1; i>=0; i--){
-            if(bucket.empty()){
-                result.push_back(0);
-                bucket.push(i);
+            if(temperatures[i] >= hottest){
+                hottest = temperatures[i];
             }
             else{
-                while(!bucket.empty() && temperatures[bucket.top()] <= temperatures[i]){
-                    bucket.pop();
+                int it = i+1;
+                while(temperatures[it] <= temperatures[i]){
+                    it = it + result[it];
                 }
-                if(bucket.empty()){
-                    result.push_back(0);
-                    bucket.push(i);
-                }
-                else{
-                    result.push_back(bucket.top() - i);
-                    bucket.push(i);
-                }
+                result[i] = it - i;
             }
         }
-        reverse(result.begin(), result.end());
         return result;
     }
 };
