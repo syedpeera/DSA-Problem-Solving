290b9cd74b7829e846a5452dde1edf197b51fd6d
syed peera saheb <peeraandroid.123@gmail.com>
2023-08-04 17:26:38 +0530

    Time: 3 ms (76.58%), Space: 10.2 MB (6.36%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,20 +4,18 @@ public:
         int n=nums.size();
         int left=0;
         int right=n-1;
+        int result = INT_MAX;
         while(left<=right){
             int mid=left+(right-left)/2;
-            int next=(mid+1)%n;
-            int prev=(mid-1+n)%n;
-            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next]){
-                return nums[mid];
-            }
-            else if(nums[left]<=nums[mid] && nums[mid]>=nums[right]){
-                left=mid+1;
+            if(nums[left] <= nums[mid]){
+                result = min(result, nums[left]);
+                left = mid + 1;
             }
             else{
-                right=mid-1;
+                result = min(result, nums[mid]);
+                right = mid - 1;
             }
         }
-        return 0;
+        return result;
     }
 };
