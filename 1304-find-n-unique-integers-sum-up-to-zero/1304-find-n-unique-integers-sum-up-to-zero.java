64a48431c3587949ba4034365cf76f0b0352a3ff
syed peera saheb <peeraandroid.123@gmail.com>
2023-08-07 23:26:09 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (88.18%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] sumZero(int n) {
-        int result[] = new int[n];
-        int j=0;
-        for(int i=1; i<=n/2; i++){
+        int[] result = new int[n];
+        int j = 0;
+        for (int i = 1; i <= n / 2; i++) {
             result[j++] = i;
             result[j++] = -i;
         }
-        if(n%2 != 0){
+        if (n % 2 != 0) {
             result[j] = 0;
         }
         return result;
