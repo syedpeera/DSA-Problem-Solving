90387053893b03d7001645b68a0e6397dff83a6e
syed peera saheb <peeraandroid.123@gmail.com>
2023-08-07 22:11:15 +0530

    Time: 0 ms, Memory: 38.7 MB - LeetHub

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr=0;
+        for(int i=2;i<=n;i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return curr;
+    }
+}
