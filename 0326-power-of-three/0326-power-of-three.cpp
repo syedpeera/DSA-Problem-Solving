d176129de460dfd31f9cdfe762abc0f4c138efcc
syed peera saheb <peeraandroid.123@gmail.com>
2023-08-11 13:01:27 +0530

    Time: 8 ms (95.24%), Space: 6 MB (23.49%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
+        // Largest power of 3 within the int range is 3^19
+        int maxPowerOfThree = 1162261467; // 3^19
+        return maxPowerOfThree % n == 0;
+    }
+};
