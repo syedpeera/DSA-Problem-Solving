36b2e735502d6afae350262f7891d8f87c88e5cf
syed peera saheb <peeraandroid.123@gmail.com>
2023-08-12 20:33:51 +0530

    Time: 5 ms (28.03%), Space: 7.6 MB (81.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,31 +5,31 @@ public:
         int n = obstacleGrid[0].size();
         vector<int> prev(n, 0);
         vector<int> curr(n, 0);
-        //vector<vector<int>> dp(m, vector<int>(n, 0));
-        prev[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
         
-        // Initialize first row using prev array
-        for(int j = 1; j < n; j++) {
-            if(obstacleGrid[0][j] == 1) {
+        prev[0] = (obstacleGrid[0][0] == 1)? 0: 1;
+        
+        for(int j=1; j<n; j++){
+            if(obstacleGrid[0][j] == 1){
                 prev[j] = 0;
-            } else {
-                prev[j] = prev[j - 1];
+            }
+            else{
+                prev[j] = prev[j-1];
             }
         }
         
-        // DP calculation
-        for(int i = 1; i < m; i++) {
-            curr[0] = obstacleGrid[i][0] == 1 ? 0 : prev[0];
-            for(int j = 1; j < n; j++) {
-                if(obstacleGrid[i][j] == 1) {
+        for(int i=1; i<m; i++){
+            curr[0] = (obstacleGrid[i][0] == 1)? 0 : prev[0];
+            for(int j=1; j<n; j++){
+                if(obstacleGrid[i][j] == 1){
                     curr[j] = 0;
-                } else {
-                    curr[j] = prev[j] + curr[j - 1];
+                }
+                else{
+                    curr[j] = prev[j] + curr[j-1];
                 }
             }
             prev = curr;
         }
         
-        return prev[n - 1];
+        return prev[n-1];
     }
 };
