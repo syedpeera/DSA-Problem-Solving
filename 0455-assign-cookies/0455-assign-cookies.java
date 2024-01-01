b8f3ef43035cd8dd8eb54a5cf32d4249bfda0846
syed peera saheb <peeraandroid.123@gmail.com>
2024-01-01 11:09:41 +0530

    Time: 14 ms (8.46%), Space: 45.3 MB (13.24%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int m = g.length;
+        int n = s.length;
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < m && j < n){
+            if(g[i] <= s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
