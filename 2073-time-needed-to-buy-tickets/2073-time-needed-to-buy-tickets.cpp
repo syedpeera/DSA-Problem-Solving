12f5222cb0d7b101eadefb4dc77663adc79e91af
syed peera saheb <peeraandroid.123@gmail.com>
2024-04-09 16:54:15 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (46.11%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        int n = tickets.size();
+        for(int i=0; i<n; i++){
+            if(i <= k){
+                time = time + min(tickets[k], tickets[i]);
+            }
+            else{
+                time = time + min(tickets[k]-1, tickets[i]);
+            }
+        }
+        return time;
+    }
+};
