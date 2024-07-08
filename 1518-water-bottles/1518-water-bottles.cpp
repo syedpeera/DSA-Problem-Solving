91cd33f0905c30387ee00405b47d1543552fdc31
syed peera saheb <peeraandroid.123@gmail.com>
2024-07-08 11:21:35 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (72.67%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange) {
+            
+            int K = numBottles / numExchange;
+
+            consumedBottles += numExchange * K;
+            numBottles -= numExchange * K;
+
+            numBottles += K;
+        }
+
+        return consumedBottles + numBottles;
+    }
+};
